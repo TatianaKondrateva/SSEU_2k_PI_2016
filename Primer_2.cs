6736c8d2d75969b20a1b949dbb3592d23041ac36
TatianaKondrateva <tati_kondrateva93@mail.ru>
2016-10-20 18:59:33 +0400

    Лекция 2. Пример 2

---
+++ Primer_2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primer_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+            int.TryParse(Console.ReadLine(), out A);
+            int.TryParse(Console.ReadLine(), out B);
+            int Max;
+            if (A >= B)
+            {
+                Max = A;
+            }
+            else
+            {
+                Max = B;
+            };
+            Console.WriteLine("Максимальное число = " + Max);
+            Console.ReadKey();
+
+        }
+    }
+}
